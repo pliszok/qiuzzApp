2efc3df88ba9cb5eb9c437c276c943b89b8e5736
Petr Liszok <liszok@email.cz>
2025-04-02 11:56:24 +0200

    QuestionController

---
+++ src/main/java/com/pliszok/quizzapp/QuestionController.java
@@ -0,0 +1,16 @@
+package com.pliszok.quizzapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @GetMapping("allQuestions")
+    public String getAllQuestions(){
+        return "This are your questions.";
+    }
+
+}
